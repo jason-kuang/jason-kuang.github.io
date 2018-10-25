744bf677d569f9fb31619dbf39956a6159e68e4b
jason-kuang <jkuang5@buffalo.edu>
2018-10-25 16:17:12 -0400

    Add files via upload

---
+++ sounds.js
@@ -0,0 +1,34 @@
+function basicSynth() {
+    var synth = new Tone.Synth().toMaster()
+    return synth;
+}
+
+function playNote(synth,note,duration,time) {
+    var synth = new Tone.Synth().toMaster()
+    time = time/4
+    synth.triggerAttackRelease(note,duration,time)
+}
+
+function playSong(synth, arrayNotes) {
+    for (x in arrayNotes) {
+        note = arrayNotes[x]["note"]
+        duration = arrayNotes[x]["duration"]
+        time = arrayNotes[x]["time"]
+        playNote(synth,note,duration,time)
+}
+}
+function razorSynth() {
+    var synthA = new Tone.Synth({
+	oscillator : {
+  	type : 'fatsawtooth',
+  },
+  envelope : {
+  	attack : 0.007,
+    decay : 0.12,
+    sustain: 0.05,
+    release: 1.0
+  }
+}).toMaster()
+}
+dict = [{"note":"G3","duration":"8n","time":5},{"note":"G3","duration":"8n","time":5.66},{"note":"G3","duration":"8n","time":6.33},{"note":"G3","duration":"4n","time":7},{"note":"E3","duration":"4n","time":9},{"note":"F3","duration":"4n","time":11},{"note":"G3","duration":"8n","time":13},{"note":"F3","duration":"8n","time":14.33},{"note":"G3","duration":"2n","time":15}] 
+playSong(basicSynth(),[{"note":"G3","duration":"8n","time":5},{"note":"G3","duration":"8n","time":5.66},{"note":"G3","duration":"8n","time":6.33},{"note":"G3","duration":"4n","time":7},{"note":"E3","duration":"4n","time":9},{"note":"F3","duration":"4n","time":11},{"note":"G3","duration":"8n","time":13},{"note":"F3","duration":"8n","time":14.33},{"note":"G3","duration":"2n","time":15}] )
